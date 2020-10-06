da060fb1f846a884275228d222431f907997c0d8
befy <alper@tabak.app>
2020-10-07 01:52:17 +0300

    Go mods files added

---
+++ go.mod
@@ -0,0 +1,8 @@
+module eksiduyuru-private-api
+
+go 1.14
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/gofiber/fiber/v2 v2.0.6
+)
--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"log"
 	"net/http"
 
+	"eksiduyuru-private-api/models"
+
 	"github.com/PuerkitoBio/goquery"
+	"github.com/gofiber/fiber/v2"
 )
 
-const EKSIDUYURU_URL = "https://www.eksiduyuru.com"
+const EksiDuyuruURL = "https://www.eksiduyuru.com"
+
+type postPreview models.PostPreview
 
 func Scrape() {
-	res, err := http.Get(EKSIDUYURU_URL)
+	res, err := http.Get(EksiDuyuruURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,12 +30,43 @@ func Scrape() {
 		log.Fatal(err)
 	}
 
-	doc.Find(".content h2").Each(func(i int, s *goquery.Selection) {
-		band := s.Text()
-		fmt.Printf("Duyuru %d: %s\n\n", i, band)
+	posts := make([]models.PostPreview, 0)
+
+	generateListPostsRequest(1, doc, &posts)
+	generateListPostsRequest(0, doc, &posts)
+
+	for _, post := range posts {
+		fmt.Println(post, "\n----\n")
+	}
+
+}
+
+func generateListPostsRequest(postType int, document *goquery.Document, posts *[]models.PostPreview) {
+	mainSelector := fmt.Sprintf(".content .entry%d", postType)
+	document.Find(mainSelector).Each(func(i int, s *goquery.Selection) {
+		title := s.Find("h2.title.closed").Text()
+		subtitle := s.Find(".bottomleft").Text()
+		id := s.Find("ul > li:nth-child(1) > a:nth-child(2)").Text()
+		post := models.PostPreview{
+			Title:    title,
+			Subtitle: subtitle,
+			ID:       id,
+			Type:     models.PostType{Type: 1},
+		}
+		*posts = append(*posts, post)
 	})
+
 }
 
 func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"success": true,
+		})
+	})
+
+	app.Listen(":3000")
 	Scrape()
 }
