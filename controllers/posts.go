4b7a8c54260b553212d75e75063e9c29b3cd6ee2
befy <alper@tabak.app>
2020-10-07 03:10:37 +0300

    Get posts endpoint served

---
+++ controllers/posts.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"eksiduyuru-private-api/helpers"
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetPostList(c *fiber.Ctx) error {
+	var doc *goquery.Document = c.Locals("doc").(*goquery.Document)
+	var scraper helpers.Scraper = c.Locals("scraper").(helpers.Scraper)
+	var err error
+
+	posts := scraper.GetPreviewPosts(doc)
+
+	if posts == nil {
+		fmt.Println("error 2 var", err)
+		c.JSON(fiber.Map{
+			"status":  "failure",
+			"message": "no_post_data",
+		})
+		return nil
+	}
+	c.JSON(fiber.Map{
+		"status": "success",
+		"data":   posts,
+	})
+	return nil
+}
